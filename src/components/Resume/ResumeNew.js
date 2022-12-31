ca3cad788ad9e96fb17f1506a89867fabe5b78e8
Nguyễn Võ Song Toàn <adamwilling.2002@gmail.com>
2022-12-31 23:14:00 +0700

    Update cv

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,13 +2,13 @@ import React, { useState, useEffect } from "react"
 import { Container, Row } from "react-bootstrap"
 import Button from "react-bootstrap/Button"
 import Particle from "../Particle"
-import pdf from "../../Assets/../Assets/AdamWilling_CV.pdf"
+import pdf from "../../Assets/../Assets/CV_FrontEndDeveloper_NguyenVoSongToan"
 import { AiOutlineDownload } from "react-icons/ai"
 import { Document, Page, pdfjs } from "react-pdf"
 import "react-pdf/dist/esm/Page/AnnotationLayer.css"
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`
 
-const resumeLink = "https://raw.githubusercontent.com/adamwilling/Portfolio/main/src/Assets/AdamWilling_CV.pdf"
+const resumeLink = "https://raw.githubusercontent.com/adamwilling/Portfolio/main/src/Assets/CV_FrontEndDeveloper_NguyenVoSongToan.pdf"
 
 function ResumeNew() {
     const [width, setWidth] = useState(1200)
@@ -31,7 +31,6 @@ function ResumeNew() {
                 <Row className="resume">
                     <Document file={resumeLink} className="d-flex flex-column justify-content-center align-items-center">
                         <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} className="col" />
-                        <Page pageNumber={2} scale={width > 786 ? 1.7 : 0.6} className="col" />
                     </Document>
                 </Row>
 
