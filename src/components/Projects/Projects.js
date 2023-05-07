dcfdbd1dfa94896d0b93f18dd62b71cd59c65d83
Nguyễn Võ Song Toàn <adamwilling.2002@gmail.com>
2023-05-07 14:14:29 +0700

    Update CV and Projects

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -3,6 +3,7 @@ import { Container, Row, Col } from "react-bootstrap"
 import ProjectCard from "./ProjectCards"
 import Particle from "../Particle"
 import cmd from "../../Assets/Projects/cmd.jpeg"
+import devCMD from "../../Assets/Projects/devcomaydorm.tech.jpeg"
 
 function Projects() {
     return (
@@ -16,10 +17,19 @@ function Projects() {
                 <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
                     <Col md={4} className="project-card">
                         <ProjectCard
+                            imgPath={devCMD}
+                            isBlog={false}
+                            title="Ký Túc Xá Cỏ May"
+                            description="KTX Cỏ May là một ký túc xá hiện đại và rộng rãi nằm trong khuôn viên của Trường Đại học Nông Lâm Thành phố Hồ Chí Minh, với tổng diện tích hơn 2.600 mét vuông, gồm bốn tầng và 54 phòng cho sinh viên. Ký túc xá nhằm mục đích cung cấp môi trường sống và học tập thuận lợi cho sinh viên nghèo có mong muốn theo đuổi giáo dục đại học và đóng góp cho sự phát triển của đất nước. Các phòng được trang bị bốn giường đôi, chăn ga miễn phí và két cá nhân, cũng như nhà tắm và các tiện nghi cơ bản khác. Ngoài ra, ký túc xá cũng cung cấp trợ cấp ăn uống hàng tuần, hỗ trợ toàn bộ học phí cho học kỳ đầu tiên và các loại hỗ trợ tài chính khác cho sinh viên có thành tích học tập tốt."
+                            demoLink="https://devcomaydorm.tech/"
+                        />
+                    </Col>
+                    <Col md={4} className="project-card">
+                        <ProjectCard
                             imgPath={cmd}
                             isBlog={false}
-                            title="Co May Dormitory"
-                            description="At Co May Dormitory, students are the main contributors to the management and operation of team activities, dormitory maintenance and repair, event organization, etc. of students as well as dormitories. Therefore, I proceeded to build a web application to manage boarding students according to the management process of Co May Dormitory."
+                            title="CMD | Ký Túc Xá Cỏ May"
+                            description="Tại KTX Cỏ May, sinh viên là người đóng góp chính trong việc quản lý, điều hành các hoạt động đoàn đội, bảo trì, sửa chữa KTX, tổ chức sự kiện,… của sinh viên cũng như của KTX. Vì vậy, em đã tiến hành xây dựng ứng dụng web để quản lý sinh viên nội trú theo quy trình quản lý của Ký túc xá Cỏ May."
                             demoLink="http://cmd.ktxcomay.com.vn/"
                         />
                     </Col>
--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,13 +2,13 @@ import React, { useState, useEffect } from "react"
 import { Container, Row } from "react-bootstrap"
 import Button from "react-bootstrap/Button"
 import Particle from "../Particle"
-import pdf from "../../Assets/CV_NguyenVoSongToan.pdf"
+import pdf from "../../Assets/NguyenVoSongToan-Frontend-Developer-CV-2023.pdf"
 import { AiOutlineDownload } from "react-icons/ai"
 import { Document, Page, pdfjs } from "react-pdf"
 import "react-pdf/dist/esm/Page/AnnotationLayer.css"
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`
 
-const resumeLink = "https://raw.githubusercontent.com/adamwilling/Portfolio/main/src/Assets/CV_NguyenVoSongToan.pdf"
+const resumeLink = "https://raw.githubusercontent.com/adamwilling/Portfolio/main/src/Assets/NguyenVoSongToan-Frontend-Developer-CV-2023.pdf"
 
 function ResumeNew() {
     const [width, setWidth] = useState(1200)
