980e58ea34fdcbb209f8f49de7f9b7b5183df30c
Nguyễn Võ Song Toàn <adamwilling.2002@gmail.com>
2023-06-07 11:33:44 +0700

    UpdateCV

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,13 +2,13 @@ import React, { useState, useEffect } from "react"
 import { Container, Row } from "react-bootstrap"
 import Button from "react-bootstrap/Button"
 import Particle from "../Particle"
-import pdf from "../../Assets/CV - Nguyễn Võ Song Toàn - FullStack Web Developer - 2023.pdf"
+import pdf from "../../Assets/CV_NguyễnVõSongToàn-FullStackWebDeveloper.pdf"
 import { AiOutlineDownload } from "react-icons/ai"
 import { Document, Page, pdfjs } from "react-pdf"
 import "react-pdf/dist/esm/Page/AnnotationLayer.css"
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`
 
-const resumeLink = "https://raw.githubusercontent.com/adamwilling/Portfolio/main/src/Assets/CV - Nguyễn Võ Song Toàn - FullStack Web Developer - 2023.pdf"
+const resumeLink = "https://raw.githubusercontent.com/adamwilling/Portfolio/main/src/Assets/CV_NguyễnVõSongToàn-FullStackWebDeveloper.pdf"
 
 function ResumeNew() {
     const [width, setWidth] = useState(1200)
